05281d6dd6c6ac6859ebfb68679241c1eed133a0
JoseJ1709 <jaramillojoseandres26@gmail.com>
2025-11-26 15:35:01 -0500

    Cambio del entrypoint.sh 5

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,14 +4,14 @@ set -e
 printenv | grep -E "VITE_|BACKEND_" || echo "No vars found"
 
 if [ -z "${BACKEND_URL}" ]; then
-  export BACKEND_URL="http://127.0.0. 1:8080"
+  export BACKEND_URL="http://127.0.0.1:8080"
 fi
 
 if [ -z "${VITE_API_URL}" ]; then
   export VITE_API_URL="${BACKEND_URL}"
 fi
 
-envsubst '\$BACKEND_URL' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default. conf
+envsubst '\$BACKEND_URL' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 envsubst '\$VITE_SUPABASE_URL \$VITE_SUPABASE_ANON_KEY \$VITE_API_URL \$BACKEND_URL' < /usr/share/nginx/html/env.template.js > /usr/share/nginx/html/env.js
 
