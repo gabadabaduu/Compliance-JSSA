495621dcb7bdc09f54b598a6a0d4e2a95b2f6c2c
Daniel Esteban Castellanos Echeverria <63326570+dece13@users.noreply.github.com>
2025-11-25 23:37:07 -0500

    se añadio el dominio actual de despliegue

--- src/Compliance.Web/Program.cs
+++ src/Compliance.Web/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "https://compliance-frontend-6oo9.onrender.com")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
